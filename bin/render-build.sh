37f48834edc53392be6070f27cd54d573e49a6f3
mike8730 <razerrush5v@gmail.com>
2025-07-07 11:26:17 +0900

    リセット修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-
-# データベースの全セッションを切断し、強制的に DROP
-psql $DATABASE_URL -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'ajaxapp_production_3ah3' AND pid <> pg_backend_pid();"
-psql $DATABASE_URL -c "DROP DATABASE IF EXISTS ajaxapp_production_3ah3;"
-
-# DB再作成
-bundle exec rake db:create
-bundle exec rake db:schema:load
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
